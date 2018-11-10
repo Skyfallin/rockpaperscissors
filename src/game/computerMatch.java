e916c55ae60bd21abc1cb3cca0534fce5b5f098d
dimetrius <dimetrius.hightower@gmail.com>
2018-11-10 00:13:37 -0700

    updated computerMatch

--- src/game/computerMatch.java
+++ src/game/computerMatch.java
@@ -7,59 +7,105 @@ public class computerMatch {
 
     public static void matchAgainstComputer() {
 
+        // create our variables
         String userChoice;
         int convertedUserChoice;
         int computerChoice;
+        int userTempScore = 0;
+        int computerTempScore = 0;
+        int numOfRounds;
 
-        userChoice = JOptionPane.showInputDialog("Please select either rock (1), paper (2), or scissors (3).");
-        convertedUserChoice = Integer.parseInt(userChoice);
+        // ask user to specify number of rounds
+        String inputRounds = JOptionPane.showInputDialog("Please specify a number of rounds. (1-5)");
+        numOfRounds = Integer.parseInt(inputRounds);
 
-        Random number = new Random();
+        // play the match until player or CPU gets 2 wins
+        for (int i = 0; i < numOfRounds; i++) {
+            while (userTempScore < 2 && computerTempScore < 2) {
 
-        // bound that stuff
-        computerChoice = 1 + number.nextInt(3);
+                // display menu screen
+                userChoice = JOptionPane.showInputDialog("Please select either rock (1), paper (2), or scissors (3).");
+                convertedUserChoice = Integer.parseInt(userChoice);
 
+                // generate a random value
+                Random rand = new Random();
 
-        switch (convertedUserChoice) {
+                // bound that stuff, add '1' so it doesn't start at 0.
+                computerChoice = 1 + rand.nextInt(3);
 
-            case 1:
-                switch (computerChoice) {
+                switch (convertedUserChoice) {
                     case 1:
-                        JOptionPane.showMessageDialog(null, "You and the computer both chose rock. The game resulted in a tie.");
+                        switch (computerChoice) {
+                            case 1:
+                                JOptionPane.showMessageDialog(null, "You and the computer both chose rock. It's a tie!");
+                                break;
+                            case 2:
+                                computerTempScore += 1;
+                                JOptionPane.showMessageDialog(null, "The computer chose paper. You lost!"
+                                        + "\nYour score is " + userTempScore + "/2"
+                                        + "\nThe computer's score is " + computerTempScore + "/2");
+                                break;
+                            case 3:
+                                userTempScore += 1;
+                                JOptionPane.showMessageDialog(null, "The computer chose scissors. You won!"
+                                        + "\nYour score is " + userTempScore + "/2"
+                                        + "\nThe computer's score is " + computerTempScore + "/2");
+                                break;
+                        }
                         break;
                     case 2:
-                        JOptionPane.showMessageDialog(null, "The computer chose paper. You lost!");
+                        switch (computerChoice) {
+                            case 1:
+                                userTempScore += 1;
+                                JOptionPane.showMessageDialog(null, "The computer chose rock. You won!"
+                                        + "\nYour score is " + userTempScore + "/2"
+                                        + "\nThe computer's score is " + computerTempScore + "/2");
+                                break;
+                            case 2:
+                                JOptionPane.showMessageDialog(null, "You and the computer both chose paper. It's a tie!");
+                                break;
+                            case 3:
+                                computerTempScore += 1;
+                                JOptionPane.showMessageDialog(null, "The computer chose scissors. You lost!"
+                                        + "\nYour score is " + userTempScore + "/2"
+                                        + "\nThe computer's score is " + computerTempScore + "/2");
+                                break;
+                        }
                         break;
                     case 3:
-                        JOptionPane.showMessageDialog(null, "The computer chose scissors. You won!");
+                        switch (computerChoice) {
+                            case 1:
+                                computerTempScore += 1;
+                                JOptionPane.showMessageDialog(null, "The computer chose rock. You lost!"
+                                        + "\nYour score is " + userTempScore + "/2"
+                                        + "\nThe computer's score is " + computerTempScore + "/2");
+                                break;
+                            case 2:
+                                userTempScore += 1;
+                                JOptionPane.showMessageDialog(null, "The computer chose paper. You won!"
+                                        + "\nYour score is " + userTempScore + "/2"
+                                        + "\nThe computer's score is " + computerTempScore + "/2");
+                                break;
+                            case 3:
+                                JOptionPane.showMessageDialog(null, "You and the computer both chose scissors. It's a tie!");
+                                break;
+                        }
                         break;
                 }
-                break;
-            case 2:
-                switch (computerChoice) {
-                    case 1:
-                        JOptionPane.showMessageDialog(null, "The computer chose rock. You won!");
-                        break;
-                    case 2:
-                        JOptionPane.showMessageDialog(null, "Both chose paper. It's a tie!");
-                        break;
-                    case 3:
-                        JOptionPane.showMessageDialog(null, "The computer chose scissors. You lost!");
-                        break;
-                }
-            case 3:
-                switch (computerChoice) {
-                    case 1:
-                        JOptionPane.showMessageDialog(null, "Computer chose rock. You lost!");
-                        break;
-                    case 2:
-                        JOptionPane.showMessageDialog(null, "Computer chose paper. You won!");
-                        break;
-                    case 3:
-                        JOptionPane.showMessageDialog(null, "Computer chose scissors. It's a tie!");
-                        break;
-                }
-                break;
+            }
+
+            // determine our winner, store their overall score
+
+
+            // reset our scores
+            userTempScore = 0;
+            computerTempScore = 0;
+
+            if (i == (numOfRounds-1)) {
+                JOptionPane.showMessageDialog(null, "Game over!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Moving on to round " + (i+2) + "!");
+            }
         }
     }
 }
