5ed7edeb76bab6db985fe74454acc943171a3a9c
Braeden Burgard <braedenburgard@gmail.com>
2018-11-10 00:38:31 -0700

    Got eem

---
+++ src/GUI/testGUI.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class testGUI extends JFrame {
+    private JPanel panel1;
+    private JLabel userSide;
+    private JButton closeButton;
+
+    public static void main(String[] args)
+    {
+        testGUI test = new testGUI();
+    }
+
+    public testGUI()
+    {
+        userSide.setIcon(new ImageIcon("C:\\Users\\braed\\IdeaProjects\\rockpaperscissors\\src\\GUI\\rock.png"));
+        setTitle("Main");
+        setVisible(true);
+        setSize(1000,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(panel1);
+        validate();
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
